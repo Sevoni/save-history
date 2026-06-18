3a29fad0c89f84f333e495b039f52ffbc230beff
Gugl <gusagleb626@gmail.com>
2026-06-18 09:16:39 +0300

    удалены неиспользуемые файлы добавлен .gitignore

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./src/main");
-exports.default = main_1.SaveHistoryPlugin;
-//# sourceMappingURL=main.js.map
